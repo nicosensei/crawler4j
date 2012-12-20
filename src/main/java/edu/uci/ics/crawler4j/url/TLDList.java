6b060b6f80f132b1ccf61aa6b629600f3d4a622e
Nicolas Giraud <nicolas.giraud@bnf.fr>
2012-12-20 17:22:53 +0100

    Formatting only

--- src/main/java/edu/uci/ics/crawler4j/url/TLDList.java
+++ src/main/java/edu/uci/ics/crawler4j/url/TLDList.java
@@ -8,28 +8,28 @@ import java.util.Set;
 
 public class TLDList {
 
-	private static Set<String> tldSet;
-	
-	public static boolean contains(String str) {
-		return tldSet.contains(str);
-	}
-	
-	static {
-		tldSet = new HashSet<String>();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(TLDList.class.getClassLoader().getResourceAsStream("tld-names.txt")));
-		String line;
-		try {
-			while ((line = reader.readLine()) != null) {
-				line = line.trim();
-				if (line.isEmpty() || line.startsWith("//")) {
-					continue;
-				}
-				tldSet.add(line);
-			}
-			reader.close();
-		} catch (IOException e) {			
-			e.printStackTrace();
-		}
-	}
-	
+    private static Set<String> tldSet;
+    
+    public static boolean contains(String str) {
+        return tldSet.contains(str);
+    }
+    
+    static {
+        tldSet = new HashSet<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(TLDList.class.getClassLoader().getResourceAsStream("tld-names.txt")));
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty() || line.startsWith("//")) {
+                    continue;
+                }
+                tldSet.add(line);
+            }
+            reader.close();
+        } catch (IOException e) {           
+            e.printStackTrace();
+        }
+    }
+    
 }
